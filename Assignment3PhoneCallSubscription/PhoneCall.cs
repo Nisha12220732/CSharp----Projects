6b2dda1c39ecae6d9d8e6e59771129db103f5e18
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-14 06:41:10 +0530

    Add files via upload
    Events And Delegates

---
+++ Assignment3PhoneCallSubscription/PhoneCall.cs
@@ -0,0 +1,44 @@
+using System;
+namespace Assignment3PhoneCallSubscription
+{
+    public class PhoneCall
+    {
+        public delegate void Notify();
+
+        public event Notify PhoneCallEvent;
+
+        public string Message
+        {
+            get;
+            private set;
+        }
+
+        private void OnSubscribe()
+        {
+            Message = "Subscribed to Call";
+        }
+
+        private void OnUnSubscribe()
+        {
+            Message = "UnSubscribed to Call";
+        }
+
+        public void MakeAPhoneCall(bool notify)
+        {
+            if (notify)
+            {
+                PhoneCallEvent = OnSubscribe;
+            }
+            else
+            {
+                PhoneCallEvent = OnUnSubscribe;
+            }
+
+            PhoneCallEvent();
+
+        }
+
+
+
+    }
+}
---
+++ Assignment3PhoneCallSubscription/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using Assignment3PhoneCallSubscription;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        PhoneCall call = new PhoneCall();
+        call.MakeAPhoneCall(true);
+        System.Console.WriteLine(call.Message);
+        call.MakeAPhoneCall(false);
+        System.Console.WriteLine(call.Message);        
+    }
+}
