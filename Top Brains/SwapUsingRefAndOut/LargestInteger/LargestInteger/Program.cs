f226cdd1060810e34bb0411fd96630f65e12ba3b
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-19 15:10:45 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/LargestInteger/LargestInteger/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter num1: ");
+        int num1 = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter num2: ");
+        int num2 = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter num3: ");
+        int num3 = Int32.Parse(Console.ReadLine());
+
+        if(num1 > num2 && num1 > num3)
+        {
+            Console.WriteLine($"{num1} is the Largest");
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            Console.WriteLine($"{num2} is the Largest");
+        }
+        else
+        {
+            Console.WriteLine($"{num3} is the Largest");
+        }
+    }
+}
