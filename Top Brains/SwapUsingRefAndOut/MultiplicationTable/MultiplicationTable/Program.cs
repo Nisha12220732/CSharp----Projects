1c017f7e9b90a7c2cbdb2f166d5ff592afdbd8a4
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-19 11:55:38 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+
+    public static int[] returnMultiplicationArray(int n,int num)
+    {
+        int[] arr = new int[n];
+        for (int i = 1; i <= n; i++)
+        {
+            arr[i-1] = i * num;
+        }
+        return arr;
+
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number you want Multiplication table of: ");
+        int num = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number upto which you want Multiplication table: ");
+        int n = Int32.Parse(Console.ReadLine());
+
+        foreach (int i in returnMultiplicationArray(n, num))
+        {
+            Console.Write(i + " ");
+        }
+    }
+    
+
+}
