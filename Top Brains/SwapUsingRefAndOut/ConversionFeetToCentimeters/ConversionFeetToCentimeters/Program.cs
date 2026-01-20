ec29971aa3cde8ccac70c064ec030430baaf0434
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-20 14:43:22 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/ConversionFeetToCentimeters/ConversionFeetToCentimeters/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Choice: ");
+        Console.WriteLine("1. Convert Foot to Centimeters");
+        Console.WriteLine("2. Convert Centimeters to Foot");
+        int choice = Int32.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                {
+                    Console.WriteLine("Measunrements in Foot");
+                    double foot = float.Parse(Console.ReadLine());
+                    double cm = Math.Round(foot * 30.48, 2);
+                    Console.WriteLine($"Measurement in Centimeters is: {cm}");
+                    break;
+                }
+
+            case 2:
+                {
+                    Console.WriteLine("Measunrements in Centimeters");
+                    double cm = float.Parse(Console.ReadLine());
+                    double foot = Math.Round(cm / 30.48, 2);
+                    Console.WriteLine($"Measurement in Centimeters is: {foot}");
+                    break;
+
+                }
+            default :
+                Console.WriteLine("Entered a Wrong Choice");
+                break;
+
+        }
+        
+
+
+    }
+}
