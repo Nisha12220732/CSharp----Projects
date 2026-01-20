f1d6b9b799a249aabd73c074e89c38df990b071d
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-20 14:51:58 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/SumOfPositiveIntegers/SumOfPositiveIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of elements in the Array: ");
+        int n = Int32.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = Int32.Parse(Console.ReadLine());
+        }
+
+        int sum = 0;
+
+        for(int i =0 ; i < arr.Length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                break;
+            }
+            else
+            {
+                if (arr[i] > 0)
+                {
+                    sum += arr[i];
+                }
+            }
+        }
+        Console.WriteLine($"Sum of the Array is: {sum}");
+    }
+}
