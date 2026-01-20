a71ccb9208355c9b46ea63a4c29fda2de7fed7ba
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-20 14:46:11 +0530

    Add files via upload

---
+++ DisplayHeight/DisplayHeight/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the height (in cm)");
+        int height = Int32.Parse(Console.ReadLine());
+        if (height < 150)
+        {
+            Console.WriteLine("Short");
+        }
+        else if(height < 180 &&  height >= 150)
+        {
+            Console.WriteLine("Average");
+        }
+        else
+        {
+            Console.WriteLine("Tall");
+        }
+    }
+}
