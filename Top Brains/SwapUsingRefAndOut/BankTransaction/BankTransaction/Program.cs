c84b22a04675491f7045d0a177395aabec593665
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-20 14:57:38 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/BankTransaction/BankTransaction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Transactions;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Initial Balance");
+        double balance = Double.Parse(Console.ReadLine());
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Enter the Transaction Amount: ");
+            double transaciton = Double.Parse(Console.ReadLine());
+            if (transaciton < balance)
+            {
+                balance -= transaciton;
+            }
+            else
+            {
+                Console.WriteLine("Transaction Amount Exceeded Balance!!");
+                Console.WriteLine("Transaction Halt!!");
+                exit = true;
+                break;
+            }
+            Console.WriteLine($"Current Balance is {balance}");
+            Console.WriteLine("Do you want to exit (true/false)");
+            bool flag = bool.Parse(Console.ReadLine());
+            if (flag)
+            {
+                exit = true;
+            }
+            Console.WriteLine($"Final balance is {balance}");
+
+        }
+    }
+}
