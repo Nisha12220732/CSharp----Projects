02b7ed83b5b84cd39c92e48b3adaeb6363d52fb9
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-01-23 23:47:33 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/GCD/GCD/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter First Number");
+        int a = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Number");
+        int b = Int32.Parse(Console.ReadLine());
+
+        for(int i = Math.Min(a, b); i > 0; i--)
+        {
+            if(a%i == 0 && b%i == 0)
+            {
+                Console.WriteLine($"GCD of {a} and {b} is {i}");
+                break;
+            }
+        }
+
+    }
+}
