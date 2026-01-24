6ba9922bb9fb943ebdf2a8b0944595171f4df822
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-01-25 01:00:10 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/InventoryNameCleanUp/InventoryNameCleanUp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    public static StringBuilder FixTheCase(StringBuilder input)
+    {
+        input[0] = char.ToUpper(input[0]);
+        for(int i = 1; i < input.Length; i++)
+        {
+            if (input[i-1] == ' ')
+            {
+              input[i] =  char.ToUpper(input[i]);
+            }
+        }
+        return input;
+    }
+    public static void Main(string[] args)
+    {
+        string product = Console.ReadLine();
+        StringBuilder productName = new StringBuilder(product);
+
+        for(int i = 0; i < productName.Length - 1; i++)
+        {
+            if(productName[i] == productName[i + 1])
+            {
+                productName.Remove(i, 1);
+                i--;
+            }
+        }
+       
+
+        Console.WriteLine(FixTheCase(productName));
+
+        
+    }
+}
