f277590a9d2f36a68879c73ee9835871c2638eb2
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-01-25 19:00:39 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/SumOfInt in ObjectArray/SumOfInt in ObjectArray/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the size of Array");
+        int n = Int32.Parse(Console.ReadLine());
+        Object[] objArr = new Object[n];
+
+        Console.WriteLine("Enter Elements of Array");
+        for(int i = 0; i < objArr.Length; i++)
+        {
+            objArr[i] = Console.ReadLine();
+        }
+
+        int sum = 0;
+        for(int i =0;  i < objArr.Length; i++)
+        {
+            if (Int32.TryParse(objArr[i].ToString(),out int res))
+            {
+                sum += res;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+} 
