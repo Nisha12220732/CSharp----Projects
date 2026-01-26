5ffa21ea23eb47e6a647f985454afdf62f4435b8
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-01-26 20:53:08 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/NotNull_DoubleArray/NotNull_DoubleArray/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the No. of Elements in array");
+        int n = Int32.Parse(Console.ReadLine());
+        double?[] arr = new double?[n];
+
+        Console.WriteLine("Enter Array Elements: ");
+        for(int i = 0; i < n; i++)
+        {
+            if (Double.TryParse(Console.ReadLine(), out double value))
+            {
+                arr[i] = value;
+            }
+            else
+            {
+                arr[i] = null; // invalid input handled
+            }
+        }
+
+        double avg = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if (arr[i].HasValue)
+            {
+                avg += arr[i].Value;
+            }
+        }
+
+        Console.WriteLine(Math.Round(avg/n,2));
+    }
+}
