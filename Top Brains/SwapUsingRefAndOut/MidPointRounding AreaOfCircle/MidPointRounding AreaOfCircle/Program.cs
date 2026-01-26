0e836b63864e8e56b5e11a938b09d1527048a7a1
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-01-26 21:46:18 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/MidPointRounding AreaOfCircle/MidPointRounding AreaOfCircle/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the radius of Circle: ");
+        double r = Double.Parse(Console.ReadLine());
+        Console.WriteLine(Math.Round(r*r*3.14,2));
+    }
+}
